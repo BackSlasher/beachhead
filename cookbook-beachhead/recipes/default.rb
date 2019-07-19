cda15a34618924c4a0dc8d01bdf084cb6cd60b83
Nitzan Raz <nitz.raz@gmail.com>
2019-07-19 16:15:01 +0300

    Ruby typo

--- cookbook-beachhead/recipes/default.rb
+++ cookbook-beachhead/recipes/default.rb
@@ -15,7 +15,7 @@ command = "cd #{beachhead_dir} && scripts/update_and_run.sh >log/latest.log 2>&1
 use_systemd = node['init_package'] == 'systemd'
 
 # TODO delete systemd units if not using it?
-if use_systemd:
+if use_systemd then
     systemd_unit_name = "beachhead"
     # TODO unit
     systemd_unit "#{systemd_unit_name}.service" do
@@ -40,7 +40,7 @@ if use_systemd:
       # Every 2 hours on the 0:45 minute
       # https://unix.stackexchange.com/a/396673
     end
-else:
+else
   cron 'beachhead-runthis' do
     minute '45'
     hour '*/2'
