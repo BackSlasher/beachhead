77851dcc10b27864b24c4fb22334367b49c09ae2
Nitzan Raz <nitz.raz@gmail.com>
2019-07-19 16:06:27 +0300

    Trying support of systemd instead of cron

--- cookbook-beachhead/recipes/default.rb
+++ cookbook-beachhead/recipes/default.rb
@@ -10,8 +10,40 @@
 # Parse envvar
 beachhead_dir = ENV['BEACHHEAD_DIR']
 
-cron 'beachhead-runthis' do
-  minute '45'
-  hour '*/2'
-  command "cd #{beachhead_dir} && scripts/update_and_run.sh >log/latest.log 2>&1"
+command = "cd #{beachhead_dir} && scripts/update_and_run.sh >log/latest.log 2>&1"
+
+use_systemd = node['init_package'] == 'systemd'
+
+# TODO delete systemd units if not using it?
+if use_systemd:
+    systemd_unit_name = "beachhead"
+    # TODO unit
+    systemd_unit "#{systemd_unit_name}.service" do
+      content <<~EOS
+      [Unit]
+      Description=Set the computer up. See #{beachhead_dir}
+
+      [Service]
+      Type=simple
+      Exec=#{command}
+      EOS
+    end
+    # TODO timer
+    systemd_unit "#{systemd_unit_name}.timer" do
+      content <<~EOS
+      [Unit]
+      Description=Runs the beachhead server. See #{beachhead_dir}
+
+      [Timer]
+      OnCalendar=00/2:45
+      EOS
+      # Every 2 hours on the 0:45 minute
+      # https://unix.stackexchange.com/a/396673
+    end
+else:
+  cron 'beachhead-runthis' do
+    minute '45'
+    hour '*/2'
+    command "cd #{beachhead_dir} && scripts/update_and_run.sh >log/latest.log 2>&1"
+  end
 end
